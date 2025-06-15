d987acba590c4bb42e659cd5b17b638eac191f58
Bernd Schlapsi <dev@bschlapsi.at>
2025-06-15 10:58:17 +0200

    Move from pip-tools to uv

--- requirements/update.sh
+++
@@ -1,5 +0,0 @@
-pip install -U pip pip-tools setuptools wheel
-
-pip-compile --upgrade --generate-hashes --output-file=requirements/requirements.txt requirements/requirements.in
-
-pip install -r requirements/requirements.txt
